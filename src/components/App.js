9384c86e0182765d8d25b531c4ff552a2b77bbf2
Parissai <parissa.jamali@gmail.com>
2018-05-23 15:20:43 +0100

    add screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './Title';
+import Tamagotchi from './Tamagotchi';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="app">
         <Title />
+        <Tamagotchi />
       </div>
     );
   }
---
+++ src/components/Screen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Screen extends React.Component {
+  render() {
+    return <div>Screen</div>;
+  }
+}
