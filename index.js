f1795f55ccc9a6f84b7852860c489593c24d4625
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-23 12:13:53 +0100

    set up react project

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import { render } from 'react-dom';
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+
+// const App = () => <h1>Hello World</h1>;
+
+// render(<App />, document.getElementById('root'));
