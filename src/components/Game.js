d569e675a701b8c539b3306c1630236e9d01f7a8
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-24 10:59:02 +0100

    add JSX game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+const Motivation = () => {
+  return <li className="motivation">💜</li>;
+};
+
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <h2> This is the Game</h2>;
+    const { name, avatar_url } = this.props;
+    return (
+      <div className="game">
+        <div className="game__username">{name}</div>
+        <img className="game__avatar" src={avatar_url} />
+        <div className="game__motivation">
+          <p className="game__motivation--key">Motivation</p>
+          <Motivation />
+        </div>
+      </div>
+    );
   }
 }
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -10,7 +10,7 @@ export default class Screen extends React.Component {
     this.state = {
       input: '',
       name: '',
-      avatar: ''
+      avatar_url: ''
     };
   }
 
@@ -21,15 +21,16 @@ export default class Screen extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
     getUserData(this.state.input).then(userData =>
       this.setState({
         name: userData.name || userData.login,
-        avatar: userData.avatar_url
+        avatar_url: userData.avatar_url
       })
     );
   };
   render() {
-    if (!this.state.name || !this.state.avatar) {
+    if (!this.state.name || !this.state.avatar_url) {
       return (
         <div className="screen">
           <Form
@@ -42,7 +43,7 @@ export default class Screen extends React.Component {
     } else {
       return (
         <div className="screen">
-          <Game />
+          <Game name={this.state.name} avatar_url={this.state.avatar_url} />
         </div>
       );
     }
